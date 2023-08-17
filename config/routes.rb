da8b7d6d10a2df45f151abb4517c316863a2fb31
takumihashimoto0218 <shimayo02180218@gmail.com>
2023-08-18 03:30:03 +0900

    routingとmodel

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :profiles, only: [:index, :create, :show, :update]
       post 'login', to: 'lances#login'
     end
   end  
